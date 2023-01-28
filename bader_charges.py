db990f3fe7ff021dab27e30e004e0bcdc6478146
Khaled Badawy <97332684+khaledb9@users.noreply.github.com>
2023-01-28 20:51:11 +0400

    Update bader_charges.py

--- bader_charges.py
+++ bader_charges.py
@@ -3,35 +3,22 @@ import numpy as np
 # Read charges from ACF.dat file
 charges = np.genfromtxt('ACF.dat', usecols=(4,), skip_footer=4, skip_header=2)
 
-# Read atom types and numbers from POSCAR file
-with open('POSCAR') as f:
+# Read ZVAL values from POTCAR file
+with open("POTCAR") as f:
     lines = f.readlines()
-    atom_types = lines[5].split()
-    atom_numbers = list(map(int,lines[6].split()))
+    zvals = [float(line.split()[-4]) for line in lines if "ZVAL" in line]
 
-# Read ZVAL from POTCAR file
-with open('POTCAR') as f:
-    zvals = {}
-    zval_found = False
-    for line in f:
-        if 'ZVAL' in line:
-            zval_found = True
-        if zval_found:
-            zvals[line.split()[0]] = float(line.split()[-4])
-            zval_found = False
+# Read atom count and species from POSCAR file
+with open("POSCAR") as f:
+    lines = f.readlines()
+    atom_count = [int(x) for x in lines[6].split()]
+    species = lines[5].split()
 
-# Create an array of ZVAL values for each atom
-zval_array = np.zeros(sum(atom_numbers))
-start_index = 0
-for i, atom in enumerate(atom_types):
-    try:
-        zval_array[start_index: start_index + atom_numbers[i]] = zvals[atom]
-    except KeyError:
-        zval_array[start_index: start_index + atom_numbers[i]] = 0
-    start_index += atom_numbers[i]
+# Create array that corresponds to ZVAL for each species
+zval_array = []
+for i in range(len(species)):
+    zval_array += [zvals[i]] * atom_count[i]
 
-# Subtract ZVAL from charges
-corrected_charges = charges - zval_array
-
-# Output new charges to new file
-np.savetxt('corrected_charges.dat', corrected_charges)
+# Subtract ZVAL from charges and output new file
+new_charges = charges - zval_array
+np.savetxt("corrected_charges.dat", new_charges, fmt='%.6f')
